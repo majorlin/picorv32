ef30550d02899e4daaed014cfa136e0ac66c64b0
majorlin <linmingjie@gmail.com>
2019-02-19 22:01:27 +0800

    set reg memory out of soc file

--- picosoc/picoram.v
+++ picosoc/picoram.v
@@ -17,13 +17,6 @@
  *
  */
 
-`ifndef PICORV32_REGS
-`ifdef PICORV32_V
-`error "picosoc.v must be read before picorv32.v!"
-`endif
-
-`define PICORV32_REGS picosoc_regs
-`endif
 
 module picoram (
 	input clk,
@@ -185,40 +178,3 @@ endmodule
 // Implementation note:
 // Replace the following two modules with wrappers for your SRAM cells.
 
-module picosoc_regs (
-	input clk, wen,
-	input [5:0] waddr,
-	input [5:0] raddr1,
-	input [5:0] raddr2,
-	input [31:0] wdata,
-	output [31:0] rdata1,
-	output [31:0] rdata2
-);
-	reg [31:0] regs [0:31];
-
-	always @(posedge clk)
-		if (wen) regs[waddr[4:0]] <= wdata;
-
-	assign rdata1 = regs[raddr1[4:0]];
-	assign rdata2 = regs[raddr2[4:0]];
-endmodule
-
-module picosoc_mem #(
-	parameter integer WORDS = 256
-) (
-	input clk,
-	input [3:0] wen,
-	input [21:0] addr,
-	input [31:0] wdata,
-	output reg [31:0] rdata
-);
-	reg [31:0] mem [0:WORDS-1] /* synthesis syn_ramstyle="block_ram" */;
-
-	always @(posedge clk) begin
-		rdata <= mem[addr];
-		if (wen[0]) mem[addr][ 7: 0] <= wdata[ 7: 0];
-		if (wen[1]) mem[addr][15: 8] <= wdata[15: 8];
-		if (wen[2]) mem[addr][23:16] <= wdata[23:16];
-		if (wen[3]) mem[addr][31:24] <= wdata[31:24];
-	end
-endmodule
